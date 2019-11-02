72d6fae2d24a2b5a5cf07fa972814b0c937c74f5
Han Hossain <ahanuf12@gmail.com>
2019-11-02 14:42:59 -0700

    print value of d8

--- examples/blinky/src/main.rs
+++ examples/blinky/src/main.rs
@@ -5,4 +5,7 @@ fn main() {
     
     let d7 = DigitalPin::new(7, &mut tristate, Direction::In);
     println!("D7 value: {}", d7.pin.get_value().unwrap());
+
+    let d8 = DigitalPin::new(8, &mut tristate, Direction::In);
+    println!("D8 value: {}", d8.pin.get_value().unwrap());
 }
